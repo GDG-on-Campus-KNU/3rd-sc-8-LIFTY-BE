9facb686281907b5d4377dbd8a35b69b2eed87e3
kwonssshyeon <kwonsh4396@gmail.com>
2024-02-05 23:56:22 +0900

    initial commit

---
+++ src/main/java/gdsc/sc8/LIFTY/LiftyApplication.java
@@ -0,0 +1,13 @@
+package gdsc.sc8.LIFTY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdsc/sc8/LIFTY/LiftyApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.sc8.LIFTY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
