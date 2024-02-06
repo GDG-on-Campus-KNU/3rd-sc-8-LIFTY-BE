df967b8654da3cb5bafd6230b12b2128ee85ccbb
Sion Shin <kmicety1@gmail.com>
2024-02-06 20:31:55 +0900

    feat: Swagger 설정

---
+++ src/main/java/gdsc/sc8/LIFTY/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package gdsc.sc8.LIFTY.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecuritySchemes({
+    @SecurityScheme(
+        name = "Refresh Token",
+        type = SecuritySchemeType.APIKEY,
+        in = SecuritySchemeIn.HEADER
+    ),
+    @SecurityScheme(
+        name = "Access Token",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+    )
+})
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+            .title("LIFTY API")
+            .description("LIFTY API 문서")
+            .version("1.0.0");
+
+        return new OpenAPI()
+            .addServersItem(new Server().url("/"))
+            .components(new Components())
+            .info(info);
+    }
+}
