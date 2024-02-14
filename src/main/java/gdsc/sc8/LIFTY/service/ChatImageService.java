4b5ba9f55816aafc58e19b9e5e7560f445fc196f
Sion Shin <kmicety1@gmail.com>
2024-02-15 01:04:10 +0900

    feat: GCS 이미지 업로드 로직 추가

--- src/main/java/gdsc/sc8/LIFTY/service/ChatImageService.java
+++ src/main/java/gdsc/sc8/LIFTY/service/ChatImageService.java
@@ -11,9 +11,11 @@ import gdsc.sc8.LIFTY.utils.gcp.DataBucketUtil;
 import jakarta.transaction.Transactional;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChatImageService {
@@ -31,16 +33,19 @@ public class ChatImageService {
         if (image.isEmpty()) {
             throw new NotFoundException(ErrorStatus.NO_IMAGE, ErrorStatus.NO_IMAGE.getMessage());
         }
+        log.info("이미지 존재");
         if (image.getContentType() == null || !image.getContentType().startsWith("image")) {
             throw new NotFoundException(ErrorStatus.INVALID_FILE_TYPE, ErrorStatus.INVALID_FILE_TYPE.getMessage());
         }
+        log.info("이미지 타입 확인 통과");
         ChatImage chatImage = uploadToBucket(user, image);
         return ImageResponseDto.convertToDto(chatImage);
 
     }
 
     private ChatImage uploadToBucket(User user, MultipartFile image) throws IOException {
-        String imageUri = IMAGE_URI_PREFIX + BUCKET_NAME + "/" + dataBucketUtil.uploadToBucket(image);
+        String imageUri = IMAGE_URI_PREFIX + dataBucketUtil.uploadToBucket(image);
+        log.info("이미지 업로드 완료");
         ChatImage chatImage = ChatImage.builder()
             .user(user)
             .imageUri(imageUri)
--- src/main/java/gdsc/sc8/LIFTY/utils/gcp/DataBucketUtil.java
+++ src/main/java/gdsc/sc8/LIFTY/utils/gcp/DataBucketUtil.java
@@ -1,45 +1,72 @@
 package gdsc.sc8.LIFTY.utils.gcp;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Component
 public class DataBucketUtil {
 
+    @Value("${spring.cloud.gcp.storage.credentials.location}")
+    private String keyFileLocation;
+
     @Value("${spring.cloud.gcp.storage.bucket}")
     private String bucketName;
 
-    private final Storage storage;
+//    private final Storage storage;
 
-    @Autowired
-    public DataBucketUtil(Storage storage) {
-        this.storage = storage;
-    }
+//    @Autowired
+//    public DataBucketUtil(Storage storage) {
+//        this.storage = storage;
+//    }
+
+//    public String uploadToBucket(MultipartFile file) throws IOException {
+//
+//        // 이미지 uuid와 파일 형식
+//        String uuid = UUID.randomUUID().toString();
+//        String ext = file.getContentType();
+//        log.info("uuid: " + uuid);
+//        log.info("bucketName: " + bucketName);
+//
+//        // GCS에 이미지 업로드
+//        log.info("GCS에 이미지 업로드");
+//        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, uuid)
+//            .setContentType(ext)
+//            .build();
+//        Blob blob = storage.create(blobInfo, file.getBytes());
+//        log.info(blob.getMediaLink());
+//        return bucketName + "/" + uuid;
+//    }
 
     public String uploadToBucket(MultipartFile file) throws IOException {
-
-        // 이미지 uuid와 파일 형식
+        InputStream keyFile = ResourceUtils.getURL(keyFileLocation).openStream();
         String uuid = UUID.randomUUID().toString();
         String ext = file.getContentType();
-        log.info("uuid: " + uuid);
-        log.info("bucketName: " + bucketName);
 
-        // GCS에 이미지 업로드
+        Storage storage = StorageOptions.newBuilder()
+            .setCredentials(GoogleCredentials.fromStream(keyFile))
+            .build()
+            .getService();
+
+        String imgUrl = "https://storage.googleapis.com/" + bucketName + "/" + uuid;
+
         BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, uuid)
             .setContentType(ext)
             .build();
         Blob blob = storage.create(blobInfo, file.getBytes());
-        log.info(blob.getMediaLink());
+
         return bucketName + "/" + uuid;
     }
-
 }
